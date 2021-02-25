0168b43772cd314fb482cc80cb656e40358568f8
Pedro Alves <pedroteixeiraalves007@gmail.com>
2021-02-25 08:41:48 -0300

    feat: create sendMail route

--- src/routes.ts
+++ src/routes.ts
@@ -1,16 +1,22 @@
 import { Router } from 'express'
+import { SendMailController } from './controllers/SendMailController'
 import { SurveysController } from './controllers/SurveysController'
 import { UserController } from './controllers/UserController'
 
 const router = Router()
 
-// USER
 const userController = new UserController()
+const surveyController = new SurveysController()
+const sendMailController = new SendMailController()
+
+// USER
 router.post('/users', userController.create)
 
 // SURVEY
-const surveyController = new SurveysController()
 router.post('/surveys', surveyController.create)
 router.get('/surveys', surveyController.show)
+
+// SEND MAIL
+router.post('/sendMail', sendMailController.execute)
 
 export { router }
