82dc06de7527dbb6c98330779b0baf9982d4ab53
Pedro Alves <pedroteixeiraalves007@gmail.com>
2021-02-26 09:10:05 -0300

    feat: create nps router

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express'
 import { AnswerController } from './controllers/AnswerController'
+import { NpsController } from './controllers/NpsController'
 import { SendMailController } from './controllers/SendMailController'
 import { SurveysController } from './controllers/SurveysController'
 import { UserController } from './controllers/UserController'
@@ -10,6 +11,7 @@ const userController = new UserController()
 const surveyController = new SurveysController()
 const sendMailController = new SendMailController()
 const answerController = new AnswerController()
+const npsController = new NpsController()
 
 // USER
 router.post('/users', userController.create)
@@ -23,5 +25,8 @@ router.post('/sendMail', sendMailController.execute)
 
 // ANSWER
 router.get('/answers/:value', answerController.execute)
+
+// NPS 
+router.get('/nps/:survey_id', npsController.execute)
 
 export { router }
