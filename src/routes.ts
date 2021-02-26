447170e77a13a96a26dbc158443c3f2cb7baef35
Pedro Alves <pedroteixeiraalves007@gmail.com>
2021-02-26 08:47:57 -0300

    feat: create answer route

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express'
+import { AnswerController } from './controllers/AnswerController'
 import { SendMailController } from './controllers/SendMailController'
 import { SurveysController } from './controllers/SurveysController'
 import { UserController } from './controllers/UserController'
@@ -8,6 +9,7 @@ const router = Router()
 const userController = new UserController()
 const surveyController = new SurveysController()
 const sendMailController = new SendMailController()
+const answerController = new AnswerController()
 
 // USER
 router.post('/users', userController.create)
@@ -18,5 +20,8 @@ router.get('/surveys', surveyController.show)
 
 // SEND MAIL
 router.post('/sendMail', sendMailController.execute)
+
+// ANSWER
+router.get('/answers/:value', answerController.execute)
 
 export { router }
